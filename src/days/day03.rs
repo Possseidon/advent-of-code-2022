ca4e2f7476217c2cae0a9efa450d933327d53cec
Possseidon <xpossseidon@gmail.com>
2022-12-07 11:29:19 +0100

    Solve Day 3.

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,7 +1,43 @@
-pub fn part1(_input: String) -> u32 {
-    todo!()
+use itertools::Itertools;
+
+pub fn part1(input: String) -> u32 {
+    input
+        .lines()
+        .map(|rucksack| {
+            let (compartment1, compartment2) = rucksack.split_at(rucksack.len() / 2);
+            item_priority(
+                compartment1
+                    .chars()
+                    .find(|&item| compartment2.contains(item))
+                    .unwrap(),
+            )
+        })
+        .sum()
 }
 
-pub fn part2(_input: String) -> u32 {
-    todo!()
+pub fn part2(input: String) -> u32 {
+    input
+        .lines()
+        .chunks(3)
+        .into_iter()
+        .map(|mut group| {
+            let (rucksack1, rucksack2, rucksack3) = group.next_tuple().unwrap();
+            item_priority(
+                rucksack1
+                    .chars()
+                    .find(|&item| rucksack2.contains(item) && rucksack3.contains(item))
+                    .unwrap(),
+            )
+        })
+        .sum()
+}
+
+fn item_priority(item: char) -> u32 {
+    let item = item.try_into().unwrap();
+    match item {
+        b'a'..=b'z' => 1 + item - b'a',
+        b'A'..=b'Z' => 27 + item - b'A',
+        _ => panic!("invalid item"),
+    }
+    .into()
 }
